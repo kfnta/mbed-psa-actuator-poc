c8f711caab2c850f74289546261e90c09153463c
Oren Cohen <oren.cohen@arm.com>
2019-03-26 13:45:35 +0200

    add second partition

--- actuator_control/COMPONENT_SPE/control_partition.cpp
+++ actuator_control/COMPONENT_SPE/control_partition.cpp
@@ -39,9 +39,49 @@ const psa_key_id_t g_key_id = 100500;
 
 typedef psa_status_t (*SignalHandler)(psa_msg_t *);
 
-void actuator_business_logic(void)
+void lazy_init()
+{
+    static int is_initialized = 0;
+
+    if (is_initialized) {
+        return;
+    }
+
+    if (PSA_SUCCESS != psa_crypto_init()) {  //make sure crypto partition is initialized
+        error("Cannot initialize crypto");
+    }
+
+    // provisioning use case
+    {
+        psa_key_handle_t key_handle;
+
+        psa_status_t status = psa_create_key(PSA_KEY_LIFETIME_PERSISTENT, g_key_id, &key_handle);
+        if (PSA_SUCCESS != status) {
+            if (PSA_ERROR_ALREADY_EXISTS != status) { // already provisioned
+                error("Failed to provision a key");
+            }
+        }
+
+        psa_key_policy_t policy = psa_key_policy_init();
+        psa_key_policy_set_usage(&policy, PSA_KEY_USAGE_DECRYPT, PSA_ALG_GCM);
+        psa_set_key_policy(key_handle, &policy);
+
+        uint8_t key_data[] = {
+            0x3d, 0xe0, 0x98, 0x74, 0xb3, 0x88, 0xe6, 0x49,
+            0x19, 0x88, 0xd0, 0xc3, 0x60, 0x7e, 0xae, 0x1f
+        };
+
+        psa_import_key(key_handle, PSA_KEY_TYPE_AES, key_data, sizeof(key_data));
+        psa_close_key(key_handle);
+    }
+
+    is_initialized = 1;
+}
+
+extern "C" void actuator_business_logic(void *ptr)
 {
     psa_key_handle_t key_handle;
+    lazy_init();
     psa_status_t status = psa_open_key(PSA_KEY_LIFETIME_PERSISTENT, g_key_id, &key_handle);
     if (PSA_SUCCESS != status) {
         error("can't load key");
@@ -50,16 +90,16 @@ void actuator_business_logic(void)
         osEvent evt = mail_box.get();
         if (evt.status == osEventMail) {
             mail_t *mail = (mail_t*)evt.value.p;
-            
+
             uint8_t plaintext[30] = { 0 };
             size_t plaintext_actual_size = 0;
             status = psa_aead_decrypt(
-                key_handle, 
-                PSA_ALG_GCM, 
-                mail->nonce, mail->nonce_size, 
-                mail->additional_data, mail->additional_data_size, 
-                mail->ciphertext, mail->ciphertext_size, 
-                plaintext, sizeof(plaintext), 
+                key_handle,
+                PSA_ALG_GCM,
+                mail->nonce, mail->nonce_size,
+                mail->additional_data, mail->additional_data_size,
+                mail->ciphertext, mail->ciphertext_size,
+                plaintext, sizeof(plaintext),
                 &plaintext_actual_size
             );
 
@@ -85,7 +125,7 @@ static psa_status_t actuator_process_packet(psa_msg_t *msg)
     ) {
         return PSA_DROP_CONNECTION;
     }
-    
+
     mail_t *mail = mail_box.alloc();
 
     if (NULL == mail) {
@@ -95,9 +135,9 @@ static psa_status_t actuator_process_packet(psa_msg_t *msg)
     mail->ciphertext_size = psa_read(msg->handle, 0, mail->ciphertext, sizeof(mail->ciphertext));
     mail->additional_data_size = psa_read(msg->handle, 1, mail->additional_data, sizeof(mail->additional_data));
     mail->nonce_size = psa_read(msg->handle, 2, mail->nonce, sizeof(mail->nonce));
-    
+
     mail_box.put(mail);
-    
+
     return PSA_SUCCESS;
 }
 
@@ -119,57 +159,16 @@ static void message_handler(psa_msg_t *msg, SignalHandler handler)
     psa_reply(msg->handle, status);
 }
 
-Thread thread;
 
-void lazy_init()
-{
-    static int is_initialized = 0;
-    
-    if (is_initialized) {
-        return;
-    }
-
-    if (PSA_SUCCESS != psa_crypto_init()) {  //make sure crypto partition is initialized
-        error("Cannot initialize crypto");
-    }
-    
-    // provisioning use case
-    {
-        psa_key_handle_t key_handle;
-        
-        psa_status_t status = psa_create_key(PSA_KEY_LIFETIME_PERSISTENT, g_key_id, &key_handle);
-        if (PSA_SUCCESS != status) {
-            if (PSA_ERROR_ALREADY_EXISTS != status) { // already provisioned
-                error("Failed to provision a key");
-            }
-        }
-
-        psa_key_policy_t policy = psa_key_policy_init();
-        psa_key_policy_set_usage(&policy, PSA_KEY_USAGE_DECRYPT, PSA_ALG_GCM);
-        psa_set_key_policy(key_handle, &policy);
-
-        uint8_t key_data[] = {
-            0x3d, 0xe0, 0x98, 0x74, 0xb3, 0x88, 0xe6, 0x49,
-            0x19, 0x88, 0xd0, 0xc3, 0x60, 0x7e, 0xae, 0x1f
-        };
-
-        psa_import_key(key_handle, PSA_KEY_TYPE_AES, key_data, sizeof(key_data));
-        psa_close_key(key_handle);
-    }
-
-    thread.start(callback(actuator_business_logic)); //spin-out business logic thread
-
-    is_initialized = 1;
-}
 
 extern "C" void actuator_control_entry_point(void *ptr)
 {
     psa_signal_t asserted_signals = 0;
     psa_msg_t msg = {0};
-    
+
     while (1) {
         asserted_signals = psa_wait(ACTUATOR_CONTROL_SRV_WAIT_ANY_SID_MSK, PSA_BLOCK);
-        lazy_init();
+
         if ((asserted_signals & ACTUATOR_CONTROL_PROCESS_PACKET_MSK) != 0) {
             if (PSA_SUCCESS != psa_get(ACTUATOR_CONTROL_PROCESS_PACKET_MSK, &msg)) {
                 continue;
--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash -ex
+rm -rf BUILD
+rm -rf mbed-os/BUILD
 cp -r  actuator_control mbed-os/components/TARGET_PSA/services
 python mbed-os/tools/psa/generate_partition_code.py
 python mbed-os/tools/psa/release.py -m CY8CKIT_062_WIFI_BT_M0_PSA -d
